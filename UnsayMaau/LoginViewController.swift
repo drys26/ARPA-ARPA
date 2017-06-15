a93fca2789ffdeae36c723db5fed7343671bb936
drys26 <dkc4148077@gmail.com>
2017-06-15 13:34:15 +0800

    Organizing Files

--- UnsayMaau/LoginViewController.swift
+++ UnsayMaau/LoginViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LoginViewController.swift
 //  UnsayMaau
 //
 //  Created by Nexusbond on 15/06/2017.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
