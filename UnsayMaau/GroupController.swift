5444443294c090760ccad90df8e3068551b1531f
jerixkiler <jerixkiller3@gmail.com>
2017-06-20 11:49:32 +0800

    added more controllers

---
+++ UnsayMaau/GroupController.swift
@@ -0,0 +1,34 @@
+//
+//  GroupController.swift
+//  UnsayMaau
+//
+//  Created by Nexusbond on 20/06/2017.
+//  Copyright © 2017 Nexusbond. All rights reserved.
+//
+
+import UIKit
+import Floaty
+
+class GroupController: UIViewController {
+
+    @IBOutlet weak var floats: Floaty!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        
+        
+    self.view.bringSubview(toFront: floats)
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
